47f218e433d59235efc2d0e82bd2ea0de87cced3
brunamilhomem99 <120478206+brunamilhomem99@users.noreply.github.com>
2023-05-14 11:25:04 -0300

    Média Aritmética

--- exercícios-curso-em-video/ex007.py
+++ exercícios-curso-em-video/ex007.py
@@ -1,10 +1,10 @@
 # Desenvolva um programa que leia as duas notas de um aluno, calcule e mostre a sua média
 
 # getting input from user
-grade1 = float(input('Digite o valor de uma nota escolar: '))
-grade2 = float(input('Digite outro: '))
+grade1 = float(input('Primeira nota escolar: '))
+grade2 = float(input('Segunda nota escolar: '))
 
 average = (grade1 + grade2) / 2
 
 # printing the result
-print('A média das duas notas é {:.1f}'.format(average))
+print('A média das duas notas é \033[4;36m{:.1f}\033[m'.format(average))
