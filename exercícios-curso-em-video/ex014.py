bf10bd54c75009163175bd29b0bbbe76924b705d
brunamilhomem99 <120478206+brunamilhomem99@users.noreply.github.com>
2023-05-14 14:26:45 -0300

    Conversor de Temperaturas

--- exercícios-curso-em-video/ex014.py
+++ exercícios-curso-em-video/ex014.py
@@ -1,4 +1,4 @@
 # Escreva um programa que leia a temperatura em graus Celcius (Cº) e converta para Fahrenheit (Fº)
 
 cel = float(input('Temperatura em celcius: '))
-print('O valor da temperatura em {}ºC equivale a {:.2f}ºF'.format(cel, cel * 1.8 + 32))
+print('O valor da temperatura em \033[33m{}ºC\033[m equivale a \033[36m{:.2f}ºF\033[m'.format(cel, cel * 1.8 + 32))
