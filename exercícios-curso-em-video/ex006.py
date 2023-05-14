6fec9269eded25ed6a4df3fa97230dd516ac783b
brunamilhomem99 <120478206+brunamilhomem99@users.noreply.github.com>
2023-05-14 11:22:44 -0300

    Dobro, Triplo e Raíz Quadrada

--- exercícios-curso-em-video/ex006.py
+++ exercícios-curso-em-video/ex006.py
@@ -4,4 +4,4 @@
 value = int(input('Digite um valor inteiro: '))
 
 # imprimindo o resultado
-print(f'O dobro é valor dado é {value*2}, o triplo é {value*3}, e a raíz quadrada é {value**(1/2)}')
+print(f'O \033[1mdobro\033[m do valor dado é \033[35m{value*2}\033[m, o \033[1mtriplo\033[m é \033[32m{value*3}\033[m, e a \033[1mraíz quadrada\033[m é \033[36m{value**(1/2)}\033[2m')
