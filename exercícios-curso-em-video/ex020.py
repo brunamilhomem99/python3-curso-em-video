39c82220c6d9a1829a11f55dbe847063592f8540
brunamilhomem99 <120478206+brunamilhomem99@users.noreply.github.com>
2023-05-14 14:38:29 -0300

    Sorteando uma ordem na lista

--- exercícios-curso-em-video/ex020.py
+++ exercícios-curso-em-video/ex020.py
@@ -2,10 +2,10 @@
 # Faça um programa que leia o nome dos quatro alunos e mostre a ordem sorteada.
 
 from random import shuffle
-n1 = str(input('Primeiro nome: '))
-n2 = str(input('Segundo nome: '))
-n3 = str(input('Terceiro aluno: '))
-n4 = str(input('Quarto aluno: '))
+n1 = str(input('\033[31mPrimeiro nome: \033[m'))
+n2 = str(input('\033[32mSegundo nome: \033[m'))
+n3 = str(input('\033[34mTerceiro aluno: \033[m'))
+n4 = str(input('\033[35mQuarto aluno: \033[m'))
 alunos = [n1, n2, n3, n4]
 shuffle(alunos)
 print('A ordem de apresentação dos alunos será: ')
