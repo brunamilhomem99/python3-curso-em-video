148ef471cd9083e1ff48782352446fb1b6b72e0d
brunamilhomem99 <120478206+brunamilhomem99@users.noreply.github.com>
2023-05-14 14:36:12 -0300

    Sorteando uma ordem na lista

--- exercícios-curso-em-video/ex019.py
+++ exercícios-curso-em-video/ex019.py
@@ -6,5 +6,5 @@ n2 = str(input('Segundo aluno: '))
 n3 = str(input('Terceiro aluno: '))
 n4 = str(input('Quarto aluno: '))
 alunos = [n1, n2, n3, n4]
-print(f'O aluno escolhido para para apagar o quadro foi: {choice(alunos)}')
+print(f'O aluno escolhido para para apagar o quadro foi: \033[34m{choice(alunos)}\033[m')
 
