d2eb3a8566fb57f7ae3ec1993d79391988a6afcd
brunamilhomem99 <120478206+brunamilhomem99@users.noreply.github.com>
2023-05-14 14:21:05 -0300

    Calculando Descontos

--- exercícios-curso-em-video/ex012.py
+++ exercícios-curso-em-video/ex012.py
@@ -2,4 +2,4 @@
 
 price = float(input('Digite o preço do produto: R$'))
 new_price = price - (price * 5 / 100)
-print(f'O valor deste produto com desconto de 5% é R${new_price}')
+print('O valor deste produto com desconto de 5% é \033[35mR${:.2f}\033[m'.format(new_price))
