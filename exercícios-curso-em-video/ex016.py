195e08e2aed9a73f545c6807f5aad14514beeb3d
brunamilhomem99 <120478206+brunamilhomem99@users.noreply.github.com>
2023-05-14 14:31:21 -0300

    Quebrando um número

--- exercícios-curso-em-video/ex016.py
+++ exercícios-curso-em-video/ex016.py
@@ -1,4 +1,4 @@
 # Crie um programa que leia um número real qualquer pelo teclado e mostre na tela a sua porção inteira.
 from math import trunc
-num = float(input('Digite um número: '))
-print(f'A porção inteira do número dado é {trunc(num)}')
+num = float(input('Digite um número quebrado: '))
+print(f'A porção inteira do número dado é \033[1m{trunc(num)}\033[m')
