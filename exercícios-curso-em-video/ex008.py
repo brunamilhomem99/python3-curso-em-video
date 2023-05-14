4ea5e3cbd1cc193211b8978455dd79c5bf14c286
brunamilhomem99 <120478206+brunamilhomem99@users.noreply.github.com>
2023-05-14 11:28:03 -0300

    Conversor de Medidas

--- exercícios-curso-em-video/ex008.py
+++ exercícios-curso-em-video/ex008.py
@@ -1,6 +1,6 @@
 # Escreva um programa que leia um valor em metros e o exiba convertido em centímetros e milímetros
 
-value = int(input('Digite um valor em metros: '))
+value = int(input('Digite um valor em \033[4mmetros\033[m: '))
 
 # printing the result
-print(f'O valor {value} em centímetros é {value*100} e em milímetros é {value*1000}')
+print(f'O valor \033[34m{value}\033[m em centímetros é \033[31m{value*100}\033[m e em milímetros é \033[32m{value*1000}\033[m')
