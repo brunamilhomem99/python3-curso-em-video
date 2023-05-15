610fb3a0bbf9fc2187df8dc5ff1d2444062017a4
brunamilhomem99 <120478206+brunamilhomem99@users.noreply.github.com>
2023-05-15 16:00:44 -0300

    Primeiro e último nome de uma pessoa

--- exercícios-curso-em-video/ex027.py
+++ exercícios-curso-em-video/ex027.py
@@ -1,6 +1,6 @@
 # Faça um programa que leia o nome completo de uma pessoa, e mostre o primeiro e o último nome separadamenete;
 # ex: input: Ana Maria de Souza. Output: primeiro = Ana, segundo = Maria
 
-n = str(input('Digite seu nome completo: ')).strip()
+n = str(input('\033[36mDigite seu nome completo: \033[m')).strip()
 split = n.split()
-print('Primeiro nome: {}\nSegundo nome: {}'.format(split[0], split[len(split)-1]))
+print('Primeiro nome: \033[1m{}\033[m\nSegundo nome: \033[1m{}\033[m'.format(split[0], split[len(split)-1]))
