e43ff3c3103005f76b623d4ef00faa92541161f3
brunamilhomem99 <120478206+brunamilhomem99@users.noreply.github.com>
2023-05-15 16:57:31 -0300

    Comparando números

--- exercícios-curso-em-video/ex038.py
+++ exercícios-curso-em-video/ex038.py
@@ -1,15 +1,13 @@
 # Escreva um programa que leia dois número inteiros e compare-os, mostrando na tela uma mensagem:
-# 0 primeiro valor é maior
-# 0 segundo valor é menor
+# 0 primeiro valor é maior;
+# 0 segundo valor é menor;
 # Não existe valor maior. Os dois são iguais.
 
-a = int(input('\033[1;37mPrimeiro valor: \033[m'))
-b = int(input('\033[1;37mSegunddo valor: \033[m'))
+a = int(input('\033[1;30mPrimeiro valor: \033[m'))
+b = int(input('\033[1;30mSegunddo valor: \033[m'))
 if a > b:
-    print('O primeiro valor é \033[4;40mmaior\033[m.')
-    print('O segundo valor é \033[4;40mmenor\033[m.')
+    print('O primeiro valor é \033[4mmaior\033[m.')
 elif b > a:
-    print('O segundo valor é \033[4;40mmaior\033[m.')
-    print('O primeiro valor é \033[4;40mmenor\033[m. ')
-elif a == b:
+    print('O segundo valor é \033[4mmaior\033[m')
+else:
     print('Os dois valores são \033[4;40miguais\033[m.')
