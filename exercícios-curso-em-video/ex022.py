d5015ea373838fd7233d981b35fbce72334adcab
brunamilhomem99 <120478206+brunamilhomem99@users.noreply.github.com>
2023-05-15 15:54:06 -0300

    Analisador de Textos

--- exercícios-curso-em-video/ex022.py
+++ exercícios-curso-em-video/ex022.py
@@ -7,5 +7,5 @@
 nome = str(input('Digite seu nome completo: '))
 no_space = nome.replace(" ", "")
 first_name = nome.split()
-print(f'\033[35mEm maiúsculo: {nome.upper()}\033[m\n\033[36mEm minúsculo: {nome.lower()}\033[m\n\033[\033[34mPossui {len(no_space)} letras\033[m\n'
-      f'\033[31mPossui {len(first_name[0])} letras no primeiro nome\033[m')
+print(f'Em maiúsculo: \033[35m{nome.upper()}\033[m\nEm minúsculo: \033[36m{nome.lower()}\033[m\nPossui \033[34m{len(no_space)}\033[m letras\n'
+      f'Possui \033[31m{len(first_name[0])}\033[m letras no primeiro nome')
