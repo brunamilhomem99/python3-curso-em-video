a46fc7138d065d7c33d4a48ded5e981c69b2156e
brunamilhomem99 <120478206+brunamilhomem99@users.noreply.github.com>
2023-05-15 15:56:56 -0300

    Procurando uma string dentro de outra

--- exercícios-curso-em-video/ex025.py
+++ exercícios-curso-em-video/ex025.py
@@ -1,4 +1,4 @@
 # Crie um programa que leia o nome de uma pessoa e diga se ela tem "SILVA" no nome.
 
 nome = str(input('Digite seu nome completo: ')).strip()
-print('Se nome tem Silva? {}'.format('silva' in nome.lower()))
+print('Se nome tem Silva? \033[34m{}\033[m'.format('silva' in nome.lower()))
