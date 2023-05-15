908b8664d0c78bb1b745499120e6b4c102e155ce
brunamilhomem99 <120478206+brunamilhomem99@users.noreply.github.com>
2023-05-15 15:55:45 -0300

    Verificando as primeiras letras de um texto

--- exercícios-curso-em-video/ex024.py
+++ exercícios-curso-em-video/ex024.py
@@ -1,4 +1,4 @@
 # Crie um programa que leia um nome de uma cidade e diga se ela começa ou não com a palavra "SANTO".
 
-c = str(input('Digite o nome de uma cidade: ')).strip()
+c = str(input('\033[30mDigite o nome de uma cidade:\033[m ')).strip()
 print(c[:5].upper() == 'SANTO')
