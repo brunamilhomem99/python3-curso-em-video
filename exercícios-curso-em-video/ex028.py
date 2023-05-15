4201dd4de1b9fcf3b1941269692af9e4670d9cd5
brunamilhomem99 <120478206+brunamilhomem99@users.noreply.github.com>
2023-05-15 16:03:57 -0300

    Jogo de adivinhação v.1.0

--- exercícios-curso-em-video/ex028.py
+++ exercícios-curso-em-video/ex028.py
@@ -4,10 +4,10 @@
 from random import randint  # biblioteca para randomizar o valor
 from time import sleep  # biblioteca para dar um delay no output
 num = randint(0, 5)
-guess = int(input('Tente adivinhar um número de 0 a 5: '))
-print('PROCESSANDO...')
+guess = int(input('Tente adivinhar um número de \033[33m0\033[m a \033[36m5\033[m: '))
+print('\033[31mPROCESSANDO...\033[m')
 sleep(2)
 if guess == num:
-    print('Parabéns, você acertou!')
+    print('\033[32mParabéns, você acertou!\033[m')
 else:
-    print(f'Que pena, você errou! O número escolhido era {num}')
+    print(f'\033[31mQue pena, você errou! O número escolhido era {num}\033[m')
