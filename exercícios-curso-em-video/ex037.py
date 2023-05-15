0ea3975e5357d4c82f5de222c40740c0a358403b
brunamilhomem99 <120478206+brunamilhomem99@users.noreply.github.com>
2023-05-15 16:50:34 -0300

    Conversor de Bases Númericas

--- exercícios-curso-em-video/ex037.py
+++ exercícios-curso-em-video/ex037.py
@@ -3,14 +3,19 @@
 # 2 para octal
 # 3 para hexadecimal
 
-n = int(input('\033[1;37mEscolha um número inteiro qualquer: \033[m'))
-b = int(input('\033[1;37mEscolha uma base de converção entre Binário (1), Octal (2) ou Hexadecimal (3): \033[m'))
+n = int(input('\033[1;30mEscolha um número inteiro qualquer: \033[m'))
+print('''Escolha uma das bases para conversão:
+\033[32m[ 1 ] BINÁRIO\033[m
+\033[33m[ 2 ] OCTAL\033[m
+\033[34m[ 3 ] HEXADECIMAL\033[m''')
+b = int(input(
+    '\033[1;30mSua opção: \033[m'))
 
-# convertendo as bases de acordo com o input do usuário
 if b == 1:
     print('O número \033[32m{}\033[m é equivalente a \033[33m{}\033[m em \033[4mBinário\033[m.'.format(n, bin(n)[2:]))
 elif b == 2:
     print('O número \033[32m{}\033[m é equivalente a \033[35m{}\033[m em \033[4mOctal\033[m.'.format(n, oct(n)[2:]))
 elif b == 3:
-    print('O número \033[32m{}\033[m é equivalente a \033[36m{}\033[m em \033[4mHexadecinaml\033[m.'.format(n,
-                                                                                                            hex(n)[2:]))
+    print('O número \033[32m{}\033[m é equivalente a \033[36m{}\033[m em \033[4mHexadecinaml\033[m.'.format(n, hex(n)[2:]))
+else:
+    print(f'\033[31mOpção inválida, tente novemente.\033[m')
