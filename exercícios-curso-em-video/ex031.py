08027adf25f6ed34f12c2b264f32e5c108a446ff
brunamilhomem99 <120478206+brunamilhomem99@users.noreply.github.com>
2023-05-15 16:12:05 -0300

    Custo da viagem

--- exercícios-curso-em-video/ex031.py
+++ exercícios-curso-em-video/ex031.py
@@ -1,8 +1,8 @@
 # Desenvolva um programa que pergunte a distância de uma viagem em km. Calcule o preço da passagem, cobrando R$0,50 por
 # km para viagens de até 200km e R$0,45 para viagens mais longas.
 
-d = float(input('Qual será a distância da viagem em km? '))
+d = float(input('Qual será a distância da viagem em \033[34mkm\033[m? '))
 if d <= 200:
-    print('Para viagens de {} Km/h, o valor da passagem será R${:.2f}'.format(d, d * 0.50))
+    print('Para viagens de \033[34m{} Km/h\033[m, o valor da passagem será \033[32mR${:.2f}\033[m'.format(d, d * 0.50))
 else:
-    print('Para viagens de {} Km/h, o valor da passagem será R${:.2f}'.format(d, d * 0.45))
+    print('Para viagens de \033[34m{} Km/h\033[m, o valor da passagem será \033[32mR${:.2f}\033[m'.format(d, d * 0.45))
