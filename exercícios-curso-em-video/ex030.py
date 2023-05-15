7501c02a0bba0bf4ca7aa1ecc00e807dfc0baceb
brunamilhomem99 <120478206+brunamilhomem99@users.noreply.github.com>
2023-05-15 16:08:25 -0300

    Par ou ímpar?

--- exercícios-curso-em-video/ex030.py
+++ exercícios-curso-em-video/ex030.py
@@ -2,6 +2,6 @@
 
 num = int(input('Digite um número inteiro: '))
 if num % 2 == 0:
-    print(f'O número {num} é par.')
+    print(f'O número \033[36m{num}\033[m é \033[4mpar\033[m.')
 else:
-    print(f'O número {num} é ímpar.')
+    print(f'O número \033[36m{num}\033[m é \033[4mímpar\033[m.')
