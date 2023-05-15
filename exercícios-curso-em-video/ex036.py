b787f8591b02014546b42ba5388b0a014d006e2e
brunamilhomem99 <120478206+brunamilhomem99@users.noreply.github.com>
2023-05-15 16:34:40 -0300

    Aprovando impréstimo

--- exercícios-curso-em-video/ex036.py
+++ exercícios-curso-em-video/ex036.py
@@ -5,12 +5,11 @@
 # Calcule o valor da prestação mensal, sabendo que ela não pode
 # exceder 30% do salário ou então o empréstimo será negado.
 
-house = float(input('\033[1;37mQual o valor da casa que você deseja comprar? \033[m'))
-salary = float(input('\033[1;37mQual é valor do seu salário? \033[m'))
-years = int(input('\033[1;37mEm quantos anos você irá pagar o imóvel? \033[m'))
-installment = house / (years * 12)  # calculando a prestação mensal
+house = float(input('\033[1;30mQual o valor da casa que você deseja comprar? R$\033[m'))
+salary = float(input('\033[1;30mQual é valor do seu salário? R$\033[m'))
+years = int(input('\033[1;30mEm quantos anos você irá pagar o imóvel? \033[m'))
+installment = house / (years * 12)
 
-# verificando se a prestação excede o salárrio do comprador
 if installment > salary * (30 / 100):
     print(
         'A prestação de \033[31;40mR${:.2f}\033[m excede mais de 30% do seu salário de \033[31mR${:.2f}\033[m. Sinto '
