6cda8ae559d25f856d4fb39fb2b5829b725e19c3
brunamilhomem99 <120478206+brunamilhomem99@users.noreply.github.com>
2023-05-15 17:30:37 -0300

    Aquele clássico da Média

--- exercícios-curso-em-video/ex040.py
+++ exercícios-curso-em-video/ex040.py
@@ -1,11 +1,13 @@
 # Crie um um programa que leia duas notas de um aluno e calcule sua média,
 # mostrando uma mensagem no final, de acordo com a média atingida
 
-a = float(input('\033[1;37mPrimeira nota: \033[m'))
-b = float(input('\033[1;37mSegunda nota: \033[m'))
+a = float(input('\033[1;30mPrimeira nota: \033[m'))
+b = float(input('\033[1;30mSegunda nota: \033[m'))
 
-average = (a + b) / 2
-if average < 5:
-    print('\033[31mVocê está de recuperação, estude mais na próxima!\033[m')
-elif average >= 5:
-    print('\033[36mParabéns, você está acima da média! Contiue assim.\033[m')
+avg = (a + b) / 2
+if 7 > avg >= 5:
+    print('\033[31mCom média {:.1f} você está de RECUPERAÇÃO, estude mais na próxima!\033[m'.format(avg))
+elif avg < 5:
+    print('\033[31mCom média {:.1f} você está REPROVADO\033[m'.format(avg))
+elif avg >= 7:
+    print('\033[36mCom média {:.1f} você está APROVADO! Contiue assim.\033[m'.format(avg))
