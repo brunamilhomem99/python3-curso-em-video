7b900416ccf1c6721f6d0d8c729c96f370aa61c5
brunamilhomem99 <120478206+brunamilhomem99@users.noreply.github.com>
2023-05-16 10:46:33 -0300

    Gerenciador de Pagamento

--- exercícios-curso-em-video/ex044.py
+++ exercícios-curso-em-video/ex044.py
@@ -5,17 +5,26 @@
 # Em até duas vezes no cartão: preço normal
 # 3x ou mais no cartão: 20% de juros
 
-print('\033[1;37m-=-\033[m' * 35)
-print('Modalidades de Pagamento: \033[36mDinheiro/cheque (1)\033[m | \033[35mCartão: (2)\033[m | \033[33mAté 2x no '
-      'Cartão (3) | \033[31m3x ou mais no Cartão (4)')
-print('\033[1;37m-=-\033[m' * 35)
-p = int(input('Escolha uma das modalidades de pagamento acima de acordo com o dígito: '))
+print('''Modalidades de Pagamento: 
+[ 1 ] \033[36mDinheiro/cheque\033[m
+[ 2 ] \033[35mCartão\033[m
+[ 3 ] \033[33mAté 2x no Cartão\033[m
+[ 4 ] \033[34m3x ou mais no Cartão\033[m''')
+
 v = float(input('Valor do produto: \033[32mR$\033[m'))
+p = int(input('Escolha uma das modalidades de pagamento acima de acordo com o dígito: '))
 if p == 1:
-      print('Com um desconto de 10%, o valor do produto será R${:.2f}'.format(v - (v * 10 / 100)))
+    total = v - (v * 10 / 100)
+    print('Com um desconto de 10%, o valor do produto será R${:.2f}'.format(total))
 elif p == 2:
-      print('Com um desconto de 5%, o valor do produto será R${:.2f}'.format(v - (v * 5 / 100)))
+    total = v - (v * 5 / 100)
+    print('Com um desconto de 5%, o valor do produto será R${:.2f}'.format(total))
 elif p == 3:
-      print(f'Em até 2x no Cartão o valor do produto continuará R${v}')
+    print(f'Em até 2x no Cartão o valor do produto continuará R${v}')
 elif p == 4:
-      print('Em até 3x no cartão serão acrescentados 20% de juros no valor total do produto.')
+    total = v + (v * 20 / 100)
+    totparc = int(input('Quantas parcelas? '))
+    parcela = total / totparc
+    print('Sua compra será parcelada em {}x de R${:.2f} com juros. No fina custará R${}'.format(totparc, parcela, total))
+else: 
+    print('\033[31mOpção inválida. Tente novamente!\033[m')
