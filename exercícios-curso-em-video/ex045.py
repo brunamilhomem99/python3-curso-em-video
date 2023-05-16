3cedfae1d990989b13499a5616656cb18613240b
brunamilhomem99 <120478206+brunamilhomem99@users.noreply.github.com>
2023-05-16 11:13:18 -0300

    Jokenpô

--- exercícios-curso-em-video/ex045.py
+++ exercícios-curso-em-video/ex045.py
@@ -1,23 +1,40 @@
 # Crie um programa que faça o pc jogar JOKENPÔ com você.
 
-import random
-print('\033[1;37m-=-\033[m' * 7)
-print('\033[33mLETS\033[m \033[35mPLAY\033[m \033[36mJOKENPÔ!\033[m')
-print('\033[1;37m-=-\033[m' * 7)
-jogada_user = str(input('Escolha entre PEDRA PAPEL ou TESOURA: ')).upper()
-elementos = ['PEDRA', 'PAPEL', 'TESOURA']
-jogada_pc = random.choice(elementos)
-if jogada_user == 'PEDRA'.upper() and jogada_pc == 'PAPEL':
-    print('\033[1;32mParabéns, você ganhou!\033[m')
-elif jogada_user == 'PAPEL'.upper() and jogada_pc == 'PEDRA':
-    print('\033[1;31mGame Over\033[m')
-elif jogada_user == 'PEDRA'.upper() and jogada_pc == 'TESOURA':
-    print('\033[1;32mParabéns, você ganhou!\033[m')
-elif jogada_user == 'TESOURA'.upper() and jogada_pc == 'PEDRA':
-    print('\033[1;31mGame Over\033[m')
-elif jogada_user == 'TESOURA'.upper() and jogada_pc == 'PAPEL':
-    print('\033[32mParabéns, você ganhou!\033[m')
-elif jogada_user == 'PAPEL'.upper() and jogada_pc == 'TESOURA':
-    print('\033[31mGame Over\033[m')
-elif jogada_user == jogada_pc:
-    print('\033[33mEMPATE!')
+from random import randint
+elementos = ('pedra', 'papel', 'tesoura')
+jogada_pc = randint(0, 2)
+jogada_user = int(input('''Escolha entre 
+[ 0 ]PEDRA 
+[ 1 ]PAPEL
+[ 2 ]TESOURA
+Sua opção: '''))
+print(f'Computador jogou {elementos[jogada_pc]}')
+print(f'Humano jogou {elementos[jogada_user]}')
+
+if jogada_pc == 0:  # computador jogou PEDRA
+    if jogada_user == 0:
+        print('EMPATE')
+    elif jogada_user == 1:
+        print('Você ganhou, PARABÉNS!')
+    elif jogada_user == 2:
+        print('\033[31mGAME OVER\033[m')
+    else:
+        print('Joagada Inválida')
+elif jogada_pc == 1:
+    if jogada_user == 0:
+        print('\033[31mGAME OVER\033[m')
+    elif jogada_user == 1:
+        print('EMPATE')
+    elif jogada_user == 2:
+        print('Você ganou, PARABENS!')
+    else: 
+        print('Jogada Inválida')
+elif jogada_pc == 2:
+    if jogada_user == 0:
+        print('Você ganhou, PARABÉNS!')
+    elif jogada_user == 1:
+        print('\033[31mGAME OVER\033[m')
+    elif jogada_user == 2:
+        print('EMPATE')
+    else:
+        print('Jogada Inválida')
