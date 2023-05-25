ab5061feeaff4dde01b06bfccfc3d79b90a27b67
brunamilhomem99 <120478206+brunamilhomem99@users.noreply.github.com>
2023-05-25 11:32:43 -0300

    Tabuada v.2.0

--- exercícios-curso-em-video/ex049.py
+++ exercícios-curso-em-video/ex049.py
@@ -1,7 +1,7 @@
 # Refaça o DESAFIO 009, mostrando a tabuada de um número que o usuário escolher, só que agora usando um loop for.
 
 n = int(input('Digite um número para ver sua tabuada: '))
-print('\033[33m=\033[m' * 10)
+print('\033[33m-\033[m' * 10)
 for c in range(1, 11):
     print(f'{n} x {c} = {n * c}')
-print('\033[33m=\033[m' * 10)
+print('\033[33m-\033[m' * 10)
