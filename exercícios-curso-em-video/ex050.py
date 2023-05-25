47da073cde5eaad0c18420e7db0dac8c621d5bbd
brunamilhomem99 <120478206+brunamilhomem99@users.noreply.github.com>
2023-05-25 11:39:18 -0300

    Soma dos pares

--- exercícios-curso-em-video/ex050.py
+++ exercícios-curso-em-video/ex050.py
@@ -2,8 +2,10 @@
 # Se o valor digitado for ÍMPAR, desconsidere-o.
 
 soma = 0
+cont = 0
 for i in range(1, 7):
-    num = int(input('Digite um número inteiro: '))
+    num = int(input('Digite um valor inteiro: '))
     if num % 2 == 0:
-        soma = soma + num
-print(f'\033[34m{soma}\033[m')
+        soma += num
+        cont += 1
+print(f'Você informou {cont} números pares e a soma foi de: \033[34m{soma}\033[m')
