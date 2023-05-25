90904433c4adaaa1c9c6abb035f9093b59593356
brunamilhomem99 <120478206+brunamilhomem99@users.noreply.github.com>
2023-05-25 14:34:23 -0300

    Números primos

--- exercícios-curso-em-video/ex052.py
+++ exercícios-curso-em-video/ex052.py
@@ -1,6 +1,15 @@
 # Faça um programa que leia um número inteiro e diga se ele é ou não um número primo.
 n = int(input('Digite um número: '))
-if n % n == 0 and n != 1:
-    print('O número é primo.')
+tot = 0
+for i in range(1, n + 1):
+    if n % i == 0:
+        print('\033[34m ', end='')
+        tot += 1
+    else:
+        print('\033[m ', end='')
+    print(f'{i}', end='')
+print(f'\n\033[mO número {n} foi divisível {tot} vezes', end='')
+if tot == 2:
+    print(', portanto \033[31mé primo\033[m.')
 else:
-    print('O número não é primo.')
+    print(', portanto \033[31mnão é primo\033[m.')
