a6756e2300cea3bdc1fd98fefbd7bf7a11fbaf7a
brunamilhomem99 <120478206+brunamilhomem99@users.noreply.github.com>
2023-05-25 11:27:57 -0300

    Soma ìmpares múltiplos de três

--- exercícios-curso-em-video/ex048.py
+++ exercícios-curso-em-video/ex048.py
@@ -1,7 +1,11 @@
 # Faça um programa que calcule a soma entre todos os números ímpares
 # que são múltiplos de três e que se encontram no intervalo de 1 até 500.
 
+soma = 0
+cont = 0
 print('\033[34mNúmeros ímpares múltiplos de 3:\033[m')
-for n in range(1, 500, 2):
+for n in range(1, 501, 2):
     if n % 3 == 0:
-        print(n)
+        cont += 1
+        soma += n
+print(f'A soma de todos os \033[31m{cont}\033[m valores solicitados é: \033[31m{soma}\033[m')
