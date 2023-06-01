8777be0f6f18bcc87d42f133d0f0420253ed5ced
brunamilhomem99 <120478206+brunamilhomem99@users.noreply.github.com>
2023-06-01 14:25:04 -0300

    Validação de dados

--- exercícios-curso-em-video/ex057.py
+++ exercícios-curso-em-video/ex057.py
@@ -1,9 +1,15 @@
 # Faça um programa que leia o gênero de uma pessoa, mas só aceite os valores 'M', 'F' ou 'NB'.
 # Caso esteja errado, peça a digitação novamente até ter um valor correto.
 
-c = 'F'
-while c == 'F':
-    genero = str(input('Com que gênero você se identifica? [M, F, NB]: ')).upper()
+# sequências de escape ANSI
+yellow = '\033[33m'
+blue = '\033[34m'
+purple = '\033[35m'
+cyan = '\033[36m'
+reset = '\033[m'
+
+while True:
+    genero = str(input(f'Com que gênero você se identifica? [{yellow}M{reset}, {blue}F{reset}, {purple}NB{reset}]: ')).strip().upper()
     if genero in ['M', 'F', 'NB']:
         break
-print('Que legal!')
+print(f'{cyan}Que legal! Seja bem vindo(a,e).{reset}')
