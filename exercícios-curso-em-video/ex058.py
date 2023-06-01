a77ac60937f77cb6dfe1f7605b846a32dae2885c
brunamilhomem99 <120478206+brunamilhomem99@users.noreply.github.com>
2023-06-01 14:45:10 -0300

    Jogo de Adivinhação v.2.0

--- exercícios-curso-em-video/ex058.py
+++ exercícios-curso-em-video/ex058.py
@@ -1,11 +1,22 @@
 # Melhore o jogo do desafio 028, em que o pc vai "pensar" em um número entre 0 e 10. Só que agora
 # o jogador vai tentar adivinhar até acertar, mostrando no final quantos palpites foram necessários para vencer.
 
+# sequências de escape ANSI
+red = '\033[31m'
+green = '\033[32m'
+reset = '\033[m'
+
 from random import randint
-c = 1
+acertou = False
 num = randint(0, 10)
-while c == 1:
+tentativas = 0
+while not acertou:
     palpite = int(input('Adivinhe o número: '))
-    if palpite == num:
+    tentativas += 1
+    if palpite < num:
+        print(f'{red}Maior{reset}. Tente outro!')
+    elif palpite > num:
+        print(f'{red}Menor{reset}. Tente outro!')
+    else:
         break
-print('Parabéns, você acertou!')
+print(f'{green}Acertou com {tentativas} tentativas. Parabéns!{reset}')
