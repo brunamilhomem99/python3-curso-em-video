ce58e974a8ae0f770d3274dc148c4293cfcfab74
brunamilhomem99 <120478206+brunamilhomem99@users.noreply.github.com>
2023-06-02 14:40:00 -0300

    Progressão Aritmética v.2.0

--- exercícios-curso-em-video/ex061.py
+++ exercícios-curso-em-video/ex061.py
@@ -1,2 +1,17 @@
 # Refaça o desafio 051. Lendo o primeiro termo e a razão de uma PA, mostrando
 # os 10 primeiros termos da progressão aritmética usando a estrutra while.
+
+blue = '\033[34m'
+red = '\033[31m'
+purple = '\033[35m'
+reset = '\033[m'
+
+p = int(input(f'{blue}Primeiro termo:{reset} '))
+r = int(input(f'{red}Razão da PA: :{reset} '))
+termo = p
+cont = 1
+while cont <= 10:
+    print(f'{termo} - ', end='')
+    termo += r
+    cont += 1
+print(f'{purple}FIM{reset}')
