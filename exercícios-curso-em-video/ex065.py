47abb8fb1dc999d73b261c61d31741747f481a32
brunamilhomem99 <120478206+brunamilhomem99@users.noreply.github.com>
2023-06-02 16:29:23 -0300

    Maior e menor valores

--- exercícios-curso-em-video/ex065.py
+++ exercícios-curso-em-video/ex065.py
@@ -1,23 +1,22 @@
 # Crie um programa que leia vários números inteiros pelo teclado. No final da execução,
-# mostre a média entre todos os valores lidos. O programa deve perguntar ao user se ele
-# quer ou não continar a digitar os valores.
+# mostre a média e qual foi foi o maior e o menor valores lidos. O programa deve perguntar ao user se ele quer ou não continar a digitar os valores.
 
-num = []
-avg = 0
-c = 1
-while c == 1:
-    n = int(input('Digite um número: '))
-    num.append(n)
-    q = str(input('Quer continuar? [S/N]: ')).upper()
-    if len(num) > 2:
-        if q == 'N':
-            avg = sum(num) / len(num)
-            print(f'A média dos valores digitados é {avg:.2f}')
-            break
-    elif q not in 'SN':
-        print('Opção ou valor inálido. Não é possível realizar a operação.')
-        break
-
+r = 'S'
+soma = quant = avg = maior = menor = 0
+while r in 'Ss':
+    num = int(input('Digite um número: '))
+    soma += num
+    quant += 1
+    if quant == 1:
+        maior = menor = num
+    else:
+        if num > maior:
+            maior = num
+        elif num < menor:
+            menor = num
+    r = str(input('Quer continuar? [S/N]: ')).strip()[0].upper()
+avg = soma / quant
+print(f'Você digitou {quant} valores. O maior foi {maior}, o menor foi {menor} e a média entre eles foi {avg}.')
 
 
 
