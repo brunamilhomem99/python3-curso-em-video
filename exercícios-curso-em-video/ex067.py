88c842b1690f21f52fe07bd6161a06d5a1352ae9
brunamilhomem99 <120478206+brunamilhomem99@users.noreply.github.com>
2023-06-09 16:29:52 -0300

    Tabuada v.3.0

--- exercícios-curso-em-video/ex067.py
+++ exercícios-curso-em-video/ex067.py
@@ -1,17 +1,20 @@
 # Faça um programa que mostre a tabuada de vários números, um de cada vez, para cada valor digitado pelo user.
 # O programa será interrompido quando o número solicitado for negativo.
 
+yellow = '\033[33m'
+reset = '\033[m'
+
 cont = num = 0
 
 while True:
     num = int(input('Digite um número: '))
 
-    print('=' * 20)
+    print(f'{yellow}={reset}' * 20)
     for i in range(1, 11):
 
         if num < 0:
             print('Programa encerrado, volte sempre!')
             break
 
-        print(f'{num} x {i} = {num * i}')
-    print('=' * 20)
+        print(f'{num} {yellow}x{reset} {i} {yellow}={reset} {num * i}')
+    print(f'{yellow}={reset}' * 20)
