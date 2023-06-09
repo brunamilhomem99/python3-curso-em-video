b438ef272e831777d5be46dee70f82f3a81eeaf6
brunamilhomem99 <120478206+brunamilhomem99@users.noreply.github.com>
2023-06-09 16:25:09 -0300

    Vários números com Flag

--- exercícios-curso-em-video/ex066.py
+++ exercícios-curso-em-video/ex066.py
@@ -2,12 +2,18 @@
 # que é a CONDIÇÃO DE PARADA (FLAG), foi digitada. No final,  mostre quantos números foram digitados e
 # qual foi a soma entre eles. (desconsiderando a flag).
 
+purple = '\033[35m'
+cyan = '\033[36m'
+reset = '\033[m'
+
 cont = num = soma = 0
 while True:
     num = int(input('Digite um número inteiro [999 para parar]: '))
+    
     if num != 999:
         cont += 1
         soma += num
     else:
         break
-print(f'Foram lidos {cont} números e a soma entre eles foi {soma}')
+
+print(f'Foram lidos {purple}{cont}{reset} números e a soma entre eles foi {cyan}{soma}{reset}')
