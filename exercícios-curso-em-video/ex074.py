648dc5917e3673c0f89b243ed8c0563fd8b27c58
brunamilhomem99 <120478206+brunamilhomem99@users.noreply.github.com>
2023-06-14 18:11:02 -0300

    Maior e menor valores em Tupla

--- exercícios-curso-em-video/ex074.py
+++ exercícios-curso-em-video/ex074.py
@@ -2,17 +2,8 @@
 # Depois disso, mostre a listagem de números gerados e também indique o menor e o amior valor que estão na tupla
 
 from random import sample
-random_numbers = sample(range(1, 11), 5)
-tupla = tuple(random_numbers)
-print(f'Números sorteados: {tupla}')
 
-valor = menor = maior = 0
-for i in tupla:
-    if valor == tupla[1]:
-        maior = menor = tupla
-    else:
-        if i < menor:
-            menor = i
-        elif i > maior:
-            maior = i
-print(f'Maior: {maior}\nMenor: {menor}')
+random_numbers = (sample(range(1, 11), 5))
+
+print(f'Números sorteados: {random_numbers}')
+print(f'Maior valor: {max(random_numbers)}\nMenor valor: {min(random_numbers)}')
