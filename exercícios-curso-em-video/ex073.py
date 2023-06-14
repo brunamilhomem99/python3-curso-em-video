851a634a184587d23c2cb691d1b84c58c29c9dc2
brunamilhomem99 <120478206+brunamilhomem99@users.noreply.github.com>
2023-06-14 18:01:31 -0300

    Tuplas com times de futebol

--- exercícios-curso-em-video/ex073.py
+++ exercícios-curso-em-video/ex073.py
@@ -5,23 +5,29 @@
 # C) Uma lista com os times em ordem alfabética;
 # D) Em que posição na tabela está o Botafogo. (adaptado para a colocação de 2023)
 
+cyan = '\033[36m'
+purple = '\033[35m'
+blue = '\033[34m'
+yellow = '\033[33m'
+green = '\033[32m'
+reset = '\033[m'
+
 colocados = ('Palmeiras', 'Flamengo', 'Cruzeiro', 'Internacional', 'Fluminense', 'Corinthians', 'Athletico-PR',
              'Atlético', 'Fortaleza', 'São Paulo', 'América', 'Botafogo', 'Santos', 'Goiás', 'Red Bull Bragantino',
              'Coritiba', 'Cuiabá', 'Grêmio', 'Vasco', 'Bahia')
 
+print(f'Lista de times do Brasileirão: {cyan}{colocados}{reset}')
+
 print('Cinco primeiros colocados: ')
 for c in range(0, 5):
-    print(colocados[c])
+    print(f'{purple}{colocados[c]}{reset}')
 
 print('\n4 últimos colocados: ')
 for c in colocados[16:]:
-    print(c)
+    print(f'{blue}{c}{reset}')
 
 print('\nTimes em ordem alfabética: ')
 for c in sorted(colocados):
-    print(c)
+    print(f'{yellow}{c}{reset}')
 
-position_botafogo = colocados.index('Botafogo')
-print(f'\nPosição do time Botafogo: {position_botafogo}º')
-
-
+print(f'\nPosição do time Botafogo: {green}{colocados.index("Botafogo") +1}º{reset}')
