0af98f0c7332c71a3773790e4cfc84b29da4eada
brunamilhomem99 <120478206+brunamilhomem99@users.noreply.github.com>
2023-06-14 17:40:13 -0300

    Números por extenso

--- exercícios-curso-em-video/ex072.py
+++ exercícios-curso-em-video/ex072.py
@@ -1,6 +1,9 @@
 # Crie um programa que tenha uma tupla totalemente preechida com contagem por extenso, de zero até vinte.
 # Seu programa deverá ler um número pelo teclado (entre 0 e 20) e mostrá-lo por extenso.
 
+red = '\033[31m'
+reset = '\033[m'
+
 numbers = ('zero', 'um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez', 'onze', 'doze',
           'treze', 'quatorze', 'quinze', 'dezesseis', 'dezessete', 'dezoito', 'dezenove', 'vinte')
 
@@ -8,5 +11,4 @@ while True:
     user_num = int(input('Digite um número de 0 a 20 para ver seu nome por extenso: '))
     if 0 <= user_num <= 20:
         break
-position = user_num
-print(numbers[position])
+print(f'Você digitou o número {red}{numbers[user_num]}{reset}')
