8ad5d9f84cf8ca0f9b03642013e588734d44fb74
brunamilhomem99 <120478206+brunamilhomem99@users.noreply.github.com>
2023-06-14 18:57:25 -0300

    Contando vogais em Tupla

--- exercícios-curso-em-video/ex077.py
+++ exercícios-curso-em-video/ex077.py
@@ -1,8 +1,14 @@
 # Crie um programa que tenha uma tupla com várias palavras (não usar acentos).
 # Depois disso, você deve mostrar, para cada palavra, quais são as suas vogais.
 
+purple = '\033[35m'
+cyan = '\033[36m'
+reset = '\033[m'
+
 palavras = ('Estrela', 'Quasar', 'Nebulosa', 'Exoplaneta', 'Gravidade', 'Luz', 'Poeira')
-for i in palavras:
-    if i in 'aeiou':
-        print(i)
+for p in palavras:
+    print(f'\nA palavra {purple}{p}{reset} possui: ', end='')
+    for letra in p:
+        if letra.lower() in 'aeiou':
+            print(f'{cyan}{letra}{reset}', end=' ')
         
